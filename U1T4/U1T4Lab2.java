b7fd012b158bf468e589f909f4b0e0a521962ea9
antoniob106 <antoniobalbuena12345@gmail.com>
2025-09-18 00:03:54 +0000

    completed U1T4Lab2.java

--- U1T4/U1T4Lab2.java
+++ U1T4/U1T4Lab2.java
@@ -4,12 +4,36 @@ public class U1T4Lab2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! You will have 20 seconds to correctly guess the next number in the pattern.");
-        Integer time = 20;
+        System.out.println("Hello! You will have to correctly guess the next number in the pattern.");
+        
         Integer baseNumber = (int)(Math.random()*11);
-        while (time > 0) {
-            
-        }
 
+        baseNumber *= 7;
+        System.out.println(baseNumber);
+        baseNumber /= 2;
+        System.out.println(baseNumber);
+        baseNumber -= 3;
+        System.out.println(baseNumber);
+        baseNumber --;
+        System.out.println(baseNumber);
+        baseNumber %= 4;
+        System.out.println(baseNumber);
+        baseNumber++;
+        System.out.println(baseNumber);
+
+        while(true) {
+            System.out.print("Next number: ");
+            Integer answer = scanner.nextInt();
+
+            if (answer==baseNumber*7) {
+                System.out.println("Great job!");
+                break;
+            }
+            else {
+                System.out.println("Not it! Try again.");
+            }
+        }
+        scanner.close();
+        System.out.println("Done!");
     }
 }
