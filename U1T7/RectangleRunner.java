db48282d597ba374f31717ccac26eb389f69dcf1
antoniob106 <antoniobalbuena12345@gmail.com>
2025-10-09 17:53:55 +0000

    seed

--- U1T7/RectangleRunner.java
+++ U1T7/RectangleRunner.java
@@ -5,16 +5,15 @@ public class RectangleRunner {
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
 
-        System.out.println("Plot 1 length: " + plot1.getLength());
-        System.out.println("Plot 1 width: " + plot1.getWidth());
-        System.out.println("Plot 1 area: " + plot1.calculateArea());
+        plot1.setWidth(75);
+        plot2.setWidth(75);
+        plot2.setLength(75);
+        plot3.setWidth(75);
 
-        System.out.println("Plot 2 length: " + plot2.getLength());
-        System.out.println("Plot 2 width: " + plot2.getWidth());
-        System.out.println("Plot 2 area: " + plot2.calculateArea());
-
-        System.out.println("Plot 3 length: " + plot3.getLength());
-        System.out.println("Plot 3 width: " + plot3.getWidth());
-        System.out.println("Plot 3 area: " + plot3.calculateArea());
+        int area1=plot1.calculateArea();
+        int area2=plot2.calculateArea();
+        int area3=plot3.calculateArea();
+        int areaSum=area1+area2+area3;
+        System.out.println("These three plots require " + areaSum + " square feet of seed");
     }
 }
