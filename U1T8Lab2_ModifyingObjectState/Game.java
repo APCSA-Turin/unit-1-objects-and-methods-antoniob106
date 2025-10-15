cff5a0dc52019cb49a1eca0dc2b22aca0537408c
antoniob106 <antoniobalbuena12345@gmail.com>
2025-10-15 17:19:05 +0000

    u1t8 lab 2

---
+++ U1T8Lab2_ModifyingObjectState/Game.java
@@ -0,0 +1,45 @@
+package U1T8Lab2_ModifyingObjectState;
+
+public class Game {
+    private String gameName;
+    private int players;
+    private int score;
+
+    public Game(String gameName, int players) {
+        this.gameName = gameName;
+        this.players = players;
+        score = 0;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public int getPlayers() {
+        return players;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addPlayer() {
+        players++;
+    }
+
+    public void increaseScore(int increase) {
+        score += increase;
+    }
+
+    public double averageScorePerPlayer() {
+        return ((double) score / players);
+    }
+
+    public boolean isGameOver() {
+        if ( score > 9 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
