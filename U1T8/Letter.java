d80f889eddc4624783477c7d213b7e4decf35b38
antoniob106 <antoniobalbuena12345@gmail.com>
2025-10-15 16:41:33 +0000

    u1t8 complete

---
+++ U1T8/Letter.java
@@ -0,0 +1,33 @@
+package U1T8;
+
+public class Letter {
+    // instance variables
+    private String sender;
+
+    // constructor; since there are no instance variables to initialize,
+    // this constructor has no parameters and has an empty body: { }
+    public Letter(String sender) {
+        this.sender = sender;
+    }
+    
+    /* add your writeLetter method here: TO BE IMPLEMENTED IN #4 */
+
+    public void greeting(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public void specialMessage() {
+        System.out.println("Java is pretty cool, wouldn't you say?");
+    }
+
+    public void closing() {
+        System.out.println("See you later!");
+    }
+
+    public void writeLetter(String name) {
+        greeting(name);
+        specialMessage();
+        closing();
+        System.out.println("From, " + sender);
+    }
+}
