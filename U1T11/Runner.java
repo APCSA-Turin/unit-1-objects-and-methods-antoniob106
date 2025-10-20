1afb9ca24a6c7fc57ed1ad204b62e4c0306ab206
antoniob106 <antoniobalbuena12345@gmail.com>
2025-10-20 18:58:49 +0000

    u1t11

---
+++ U1T11/Runner.java
@@ -0,0 +1,16 @@
+package U1T11;
+
+class Runner {
+    public static void main(String[] args) {
+        // testing BabyParrot class
+        BabyParrot baby = new BabyParrot("Toucan", 3, 4.25);
+        baby.setName("Lil' Beans");
+        String info = baby.parrotInfo();
+        System.out.println(info);
+        // testing Student class
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+    }
+}
--- U1T11/testing.java
+++
@@ -1,7 +0,0 @@
-package U1T11;
-
-class testing {
-    public static void main(String[] args) {
-        
-    }
-}
